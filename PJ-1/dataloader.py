c16bccd2984806b6ced53b2d02b8a80c1c205699
babohhauru <asukashinn412@gmail.com>
2021-11-15 21:53:42 +0800

    upload dataloader and utils files

--- PJ-1/dataloader.py
+++ PJ-1/dataloader.py
@@ -1,1 +1,33 @@
 from torch.utils.data import Dataset
+
+
+class SSTDataSet(Dataset):
+    def __init__(self, path):
+        self.data = []
+        with open(path, 'r', encoding='utf-8') as f:
+            for line in f.readlines()[1:]:
+                raw_words, target = line.strip().split('\t')
+                # simple preprocessing
+                if raw_words.endswith("\""):
+                    raw_words = raw_words[:-1]
+                if raw_words.startswith('"'):
+                    raw_words = raw_words[1:]
+                raw_words = raw_words.replace('""', '"')
+                raw_words = raw_words.split(' ')
+                self.data.append({
+                    'raw_words': raw_words,
+                    'target': int(target),
+                })
+        print("# samples: {}".format(len(self.data)))
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def convert_word_to_ids(self, vocab):
+        for i in range(len(self.data)):
+            ins = self.data[i]
+            word_ids =  [vocab[x] for x in ins['raw_words']]
+            self.data[i]['input_ids'] = word_ids
---
+++ PJ-1/utils.py
@@ -0,0 +1,17 @@
+import torch
+
+def custom_collate(batch):
+    ''' padding '''
+
+    DEFAULT_PADDING_LABEL = 0
+    input_ids, targets = [], []
+    for x in batch:
+        input_ids.append(x['input_ids'])
+        targets.append(x['target'])
+    max_len = max(len(x) for x in input_ids)
+    batch_input_ids = [x + [DEFAULT_PADDING_LABEL]\
+                  * (max_len - len(x)) for x in input_ids]
+    batch_input_ids = torch.LongTensor(batch_input_ids)
+    batch_targets = torch.LongTensor(targets)
+
+    return batch_input_ids, batch_targets
